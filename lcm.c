fa423ed83989da873d31e180a2e001609ebe3573
WGlick <wolfey@vt.edu>
2018-02-07 16:41:49 -0500

    Add files via upload

---
+++ lcm.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int gcd(int a, int b) {
+int status;
+
+if (a == b) {
+status = 1;
+
+}
+
+if (b==0) {
+status = a;
+
+}
+else {
+
+if (a % b == 0) {
+status = b;
+}
+else {
+int rem=a % b;
+status = gcd(b,rem);
+}
+}
+return status;
+}
+
+void main() {
+int b;
+int a;
+printf("Enter the first number:");
+scanf("%d",&a);
+printf("Enter the second number:");
+scanf("%d",&b);
+
+int answer = gcd(b,a);
+int lcm = (b * a) / (answer);
+
+
+printf("The least common multiple of %d and %d is %d\n",a,b,lcm);
+
+
+}
